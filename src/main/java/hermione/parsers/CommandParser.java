0d79bd5d033aba2b28c0a4a58f3c3fe9c8712105
jovnc <95868357+jovnc@users.noreply.github.com>
2025-08-21 15:05:31 +0800

    docs: Add JavaDoc comments for methods in parsers and storage classes

--- src/main/java/hermione/parsers/CommandParser.java
+++ src/main/java/hermione/parsers/CommandParser.java
@@ -5,6 +5,19 @@ import hermione.exceptions.InvalidCommandException;
 import hermione.storage.TaskStorage;
 
 public class CommandParser {
+
+    /**
+     * Parses the command and argument to create the appropriate Command object.
+     * This method uses a switch statement to determine the type of command based on the input strings.
+     * It creates and returns a specific Command object corresponding to the command type.
+     * If the command is not recognized, it throws an InvalidCommandException.
+     *
+     * @param command Command string to parse.
+     * @param argument Argument string associated with the command.
+     * @param storage TaskStorage instance used to manage tasks.
+     * @return Command object corresponding to the parsed command.
+     * @throws InvalidCommandException If the command is not recognized.
+     */
     public static Command parse(String command, String argument, TaskStorage storage) {
         return switch (command) {
             case "deadline" -> new DeadlineCommand(storage, argument);
--- src/main/java/hermione/storage/CsvTaskStorage.java
+++ src/main/java/hermione/storage/CsvTaskStorage.java
@@ -16,15 +16,34 @@ public class CsvTaskStorage implements TaskStorage {
     private static final TaskSerializer taskSerializer = new TaskSerializer();
     private TaskList tasks;
 
+    /**
+     * Constructor for CsvTaskStorage.
+     * Initializes the storage with the specified file path and loads existing tasks.
+     *
+     * @param filePath The path to the CSV file where tasks are stored.
+     */
     public CsvTaskStorage(String filePath) {
         this.filePath = Paths.get(filePath);
         this.tasks = loadTasks();
     }
 
+    /**
+     * Returns the current list of tasks.
+     * This method retrieves the tasks stored in this storage instance.
+     *
+     * @return TaskList containing all tasks.
+     */
     public TaskList getTasks() {
         return this.tasks;
     }
 
+    /**
+     * Loads tasks from the CSV file.
+     * This method reads all lines from the file, parses them into Task objects,
+     * and returns a TaskList containing these tasks.
+     *
+     * @return TaskList containing all tasks loaded from the file.
+     */
     public TaskList loadTasks() {
         List<String> lines = FileUtils.readAllLines(this.filePath);
         List<Task> tasks = lines.stream()
@@ -34,6 +53,13 @@ public class CsvTaskStorage implements TaskStorage {
         return new TaskList(tasks);
     }
 
+    /**
+     * Saves the current list of tasks to the CSV file.
+     * This method serializes each task in the TaskList and writes them to the file.
+     * If the file does not exist, it will be created.
+     *
+     * @param tasks The TaskList containing tasks to be saved.
+     */
     public void saveTasks(TaskList tasks) {
         List<String> lines = tasks.getTasks()
                                 .stream()
@@ -43,11 +69,25 @@ public class CsvTaskStorage implements TaskStorage {
         this.tasks = loadTasks();
     }
 
+    /**
+     * Adds a new task to the storage.
+     * This method appends the task to the current TaskList and saves the updated list to the file.
+     *
+     * @param task The Task object to be added to the storage.
+     */
     public void addTask(Task task) {
         tasks.addTask(task);
         saveTasks(tasks);
     }
 
+    /**
+     * Deletes a task from the storage.
+     * This method removes the task at the specified index from the TaskList and saves the updated
+     * list to the file.
+     *
+     * @param index The index of the task to be deleted.
+     * @return The Task object that was deleted.
+     */
     public Task deleteTask(int index) {
         Task task = tasks.getTask(index);
         tasks.deleteTask(task);
@@ -55,6 +95,14 @@ public class CsvTaskStorage implements TaskStorage {
         return task;
     }
 
+    /**
+     * Sets the completion status of a task.
+     * This method updates the completion status of the specified task and saves the updated
+     * TaskList to the file.
+     *
+     * @param task The Task object whose completion status is to be updated.
+     * @param isComplete The new completion status of the task (true for complete, false for incomplete).
+     */
     public void setTaskCompletion(Task task, boolean isComplete) {
         task.setCompleted(isComplete);
         saveTasks(tasks);
