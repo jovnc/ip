a694c470f38db086f3e6e7bfbafe052bd5c82e49
jovnc <95868357+jovnc@users.noreply.github.com>
2025-08-29 21:37:58 +0800

    fix: Make Hermione class final

--- src/main/java/hermione/ui/fx/Main.java
+++ src/main/java/hermione/ui/fx/Main.java
@@ -14,7 +14,7 @@ import javafx.stage.Stage;
  */
 public class Main extends Application {
 
-    private Hermione hermione = new Hermione("data/tasks.csv");
+    private final Hermione hermione = new Hermione("data/tasks.csv");
 
     @Override
     public void start(Stage stage) {
