7f1ef62ed1e0d3aed3341b796fffb9543a9c12c5
jovnc <95868357+jovnc@users.noreply.github.com>
2025-08-31 21:58:36 +0800

    feat: Add HelpCommand to give help to users
    Users do not have any documentation to see what commands are available to use in the chatbot.
    
    Let's,
    * add help command to show all available commands to users

---
+++ src/main/java/hermione/commands/HelpCommand.java
@@ -0,0 +1,43 @@
+package hermione.commands;
+
+import hermione.storage.TaskStorage;
+
+/**
+ * Represents a command to display help information in the Hermione application.
+ */
+public class HelpCommand extends Command {
+
+    /**
+     * Constructor for the HelpCommand class.
+     *
+     * @param storage  The TaskStorage instance used to manage tasks.
+     * @param argument The argument string that contains the command details.
+     */
+    public HelpCommand(TaskStorage storage, String argument) {
+        super(storage, argument);
+    }
+
+    /**
+     * Executes the command to display help information.
+     * This method returns a string containing a list of available commands
+     * and their descriptions.
+     *
+     * @return A string containing help information for the user.
+     */
+    @Override
+    public String execute() {
+        return """
+                Available commands:
+                1. help - Show this help message
+                2. exit - Exit the application
+                3. list - List all items
+                4. todo {description} - Add a new ToDo task
+                5. deadline {description} /by {date} - Add a new Deadline task
+                6. event {description} /from {start date} /to {end date} - Add a new Event task
+                7. mark {task number} - Mark a task as completed
+                8. unmark {task number} - Unmark a task as not completed
+                9. delete {task number} - Delete a task
+                10. find {keyword} - Find tasks containing the keyword
+                """;
+    }
+}
